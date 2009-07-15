534ff349b57f642eb85b01c99511fd31e1f78a46
Torsten Schönebaum <torsten.schoenebaum@planquadrat-software.de>
2009-07-15 13:28:28 +0200

    fix bug in Tag: tag use name and not tag as attribute

--- lib/fotolia/tag.rb
+++ lib/fotolia/tag.rb
@@ -1,11 +1,15 @@
 module Fotolia
   class Tag
-    attr_reader :tag
+    attr_reader :name
     attr_reader :popularity
 
     def initialize(attributes)
-      @tag = attributes['tag']
+      @name = attributes['name']
       @popularity = attributes['popularity'].to_i
+    end
+
+    def to_s
+      @name.to_s
     end
   end
 end
