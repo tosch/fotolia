c9538aebd497f66513f61343ccc88215372a9694
Torsten Schönebaum <torsten.schoenebaum@planquadrat-software.de>
2009-07-15 13:25:25 +0200

    add #media methods in ConceptualCategory and RepresentativeCategory

--- lib/fotolia/conceptual_category.rb
+++ lib/fotolia/conceptual_category.rb
@@ -3,5 +3,15 @@ module Fotolia
     def child_categories
       @fotolia.conceptual_categories.find(self)
     end
+
+    #
+    # Searches for media in this category. For the options hash, see
+    # Fotolia::Base#search.
+    #
+    # Returns a Fotolia::SearchResultSet.
+    #
+    def media(options = {})
+      @fotolia.search(options.merge({:conceptual_category => self}))
+    end
   end
 end
