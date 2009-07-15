680b9e4ee8677725c25b7c40837f01cbba9adc39
Torsten Schönebaum <torsten.schoenebaum@planquadrat-software.de>
2009-07-15 13:29:06 +0200

    implement Galleries#create method

--- lib/fotolia/galleries.rb
+++ lib/fotolia/galleries.rb
@@ -4,15 +4,28 @@ module Fotolia
       @fotolia = fotolia_client
     end
 
+    #
+    # Returns public galleries in an array.
+    #
     def find_all
       rsp = @fotolia.remote_call('getGalleries', @fotolia.language.id)
 
       rsp.collect{|g| Fotolia::Gallery.new(@fotolia, g)}
     end
 
-    # TODO:: implement function
+    #
+    # Creates a gallery for the logged in user.
+    #
+    # Requires an authenticated session, see Fotolia::Base#login.
+    #
+    # Not working with Partner API.
+    #
     def create(name)
       raise Fotolia::LoginRequiredError unless @fotolia.logged_in?
+
+      res = @fotolia.remote_call('createUserGallery', @fotolia.session_id, name)
+
+      Fotolia::Gallery.new(@fotolia, {'id' => res['id'], 'name' => name})
     end
   end
 end
