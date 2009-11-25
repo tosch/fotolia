f4a5b24c7db542dabe188fc4f75f864846797cc9
Torsten Schönebaum <torsten.schoenebaum@planquadrat-software.de>
2009-11-25 11:38:35 +0100

    use patron as http client library by monkey patching ruby's XML-RPC implementation

--- lib/fotolia.rb
+++ lib/fotolia.rb
@@ -22,17 +22,88 @@ require 'xmlrpc/client'
   'user'
 ].each {|file| require File.join(File.dirname(__FILE__), 'fotolia', file)}
 
+require 'patron'
 
 #
 # Monkey patch XMLRPC::Client as Fotolia returns application/xml as content type
 # instead of text/xml which XMLRPC expects
+#
+# Another monkey patch so that XMLRPC::Client will use the much faster Patron
+# http client
 #
 class XMLRPC::HTTPException < Exception #:nodoc:
 end
 class XMLRPC::HTTPAuthenticationException < Exception #:nodoc:
 end
 class XMLRPC::Client #:nodoc:
+  def initialize(host=nil, path=nil, port=nil, proxy_host=nil, proxy_port=nil,
+      user=nil, password=nil, use_ssl=nil, timeout=nil)
+
+    @http_header_extra = nil
+    @http_last_response = nil
+    @cookie = nil
+
+    @host       = host || "localhost"
+    @path       = path || "/RPC2"
+    @proxy_host = proxy_host
+    @proxy_port = proxy_port
+    @proxy_host ||= 'localhost' if @proxy_port != nil
+    @proxy_port ||= 8080 if @proxy_host != nil
+    @use_ssl    = use_ssl || false
+    @timeout    = timeout || 30
+
+    @port = port || (use_ssl ? 443 : 80)
+
+    @user, @password = user, password
+
+    # convert ports to integers
+    @port = @port.to_i unless @port.nil?
+    @proxy_port = @proxy_port.to_i unless @proxy_port.nil?
+
+    # HTTP object for synchronous calls
+    @http = build_http_client
+    @http.handle_cookies
+
+    @parser = nil
+    @create = nil
+  end
+
+  def timeout=(new_timeout)
+    @timeout = new_timeout
+    @http.timeout = @timeout
+  end
+
   private
+
+  def set_auth
+    if(@user.nil?)
+      @http.username = nil
+      @http.password = nil
+    else
+      @http.username = @user
+      @http.password = @password
+    end
+  end
+
+  def build_http_client
+    http = Patron::Session.new
+    require 'uri'
+    http.base_url = URI::HTTP.build(
+      :scheme => @use_ssl ? 'https' : 'http',
+      :host   => @host,
+      :port   => @port
+    ).to_s
+    http.timeout = @timeout
+    unless(@proxy_host.nil?)
+      http.proxy = URI::HTTP.build(:scheme => 'http', :host => @proxy_host, :port => @proxy_port).to_s
+    end
+    unless(@user.nil?)
+      http.username = @user
+      http.password = @password
+    end
+
+    http
+  end
 
   def do_rpc(request, async=false) #:nodoc:
     header = {
@@ -42,73 +113,45 @@ class XMLRPC::Client #:nodoc:
       "Connection"     => (async ? "close" : "keep-alive")
     }
 
-    header["Cookie"] = @cookie        if @cookie
     header.update(@http_header_extra) if @http_header_extra
-
-    if @auth != nil
-      # add authorization header
-      header["Authorization"] = @auth
-    end
 
     resp = nil
     @http_last_response = nil
 
-    if async
+    resp = if async
       # use a new HTTP object for each call
-      Net::HTTP.version_1_2
-      http = Net::HTTP.new(@host, @port, @proxy_host, @proxy_port)
-      http.use_ssl = @use_ssl if @use_ssl
-      http.read_timeout = @timeout
-      http.open_timeout = @timeout
+      http = build_http_client
 
       # post request
-      http.start {
-        resp = http.post2(@path, request, header)
-      }
+      http.post(@path, request, header)
     else
       # reuse the HTTP object for each call => connection alive is possible we
-      # must start connection explicitely first time so that http.request does
-      # not assume that we don't want keepalive
-      @http.start if not @http.started?
 
       # post request
-      resp = @http.post2(@path, request, header)
+      @http.post(@path, request, header)
     end
 
     @http_last_response = resp
 
     data = resp.body
 
-    if resp.code == "401"
+    if resp.status == 401
       # Authorization Required
-      raise XMLRPC::HTTPAuthenticationException, "Authorization failed.\nHTTP-Error: #{resp.code} #{resp.message}"
-    elsif resp.code[0,1] != "2"
-      raise XMLRPC::HTTPException, "HTTP-Error: #{resp.code} #{resp.message}"
+      raise XMLRPC::HTTPAuthenticationException, "Authorization failed.\nHTTP-Error: #{resp.status} #{resp.status_line}"
+    elsif resp.status < 200 or resp.status >= 300
+      raise XMLRPC::HTTPException, "HTTP-Error: #{resp.status} #{resp.message}"
     end
 
-    ct = parse_content_type(resp["Content-Type"]).first
+    ct = parse_content_type(resp.headers["Content-Type"]).first
     if (ct != "text/xml")  && (ct != "application/xml")
-      if ct == "text/html"
-        raise "Wrong content-type (received '#{ct}' but expected 'text/xml'): \n#{data}"
-      else
-        raise "Wrong content-type (received '#{ct}' but expected 'text/xml')"
-      end
+      raise "Wrong content-type (received '#{ct}' but expected 'text/xml')"
     end
 
-    expected = resp["Content-Length"] || "<unknown>"
+    expected = resp.headers["Content-Length"] || "<unknown>"
     if data.nil? or data.size == 0
       raise XMLRPC::HTTPException, "Wrong size. Was #{data.size}, should be #{expected}"
     elsif expected != "<unknown>" and expected.to_i != data.size and resp["Transfer-Encoding"].nil?
       raise XMLRPC::HTTPException, "Wrong size. Was #{data.size}, should be #{expected}"
-    end
-
-    set_cookies = resp.get_fields("Set-Cookie")
-    if set_cookies and !set_cookies.empty?
-      require 'webrick/cookie'
-      @cookie = set_cookies.collect do |set_cookie|
-        cookie = WEBrick::Cookie.parse_set_cookie(set_cookie)
-        WEBrick::Cookie.new(cookie.name, cookie.value).to_s
-      end.join("; ")
     end
 
     return data
